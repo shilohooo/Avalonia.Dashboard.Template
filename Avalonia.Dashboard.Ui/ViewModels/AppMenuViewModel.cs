f5cb6a7207f58b84963c997d69cbedfd2f627ddc
shiloh <lixiaolei595@gmail.com>
2025-06-20 16:11:05 +0800

    refactor: Wrapping the sub menu as a separate control

---
+++ Avalonia.Dashboard.Ui/ViewModels/AppMenuViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Avalonia.Dashboard.Ui.ViewModels;
+
+/// <summary>
+///     view model of <see cref="Avalonia.Dashboard.Ui.Controls.AppMenu" />
+/// </summary>
+public class AppMenuViewModel : ViewModelBase
+{
+}
