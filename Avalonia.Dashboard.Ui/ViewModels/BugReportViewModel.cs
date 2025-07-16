ce48bd9eaab934f66111cb882106a6b6db412ab1
shiloh <lixiaolei595@gmail.com>
2025-07-16 17:49:17 +0800

    feat: add bug report button
    1.can open bug report page

--- Avalonia.Dashboard.Ui/ViewModels/BugReportViewModel.cs
+++ Avalonia.Dashboard.Ui/ViewModels/BugReportViewModel.cs
@@ -1,8 +1,30 @@
-﻿namespace Avalonia.Dashboard.Ui.ViewModels;
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
+
+namespace Avalonia.Dashboard.Ui.ViewModels;
 
 /// <summary>
 ///     View model of <see cref="Views.BugReportView" />
 /// </summary>
-public class BugReportViewModel : ViewModelBase
+public partial class BugReportViewModel(IConfiguration configuration) : ViewModelBase
 {
+    private string BugReportUrl => configuration["Links:BugReport"]!;
+
+    #region Commands
+
+    /// <summary>
+    ///     Use default browser to open bug report page
+    /// </summary>
+    [RelayCommand]
+    private void OpenBugReportPage()
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = BugReportUrl,
+            UseShellExecute = true
+        });
+    }
+
+    #endregion
 }
