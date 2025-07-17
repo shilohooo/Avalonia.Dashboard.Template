1dba55ae2b973743ec1a1d07ece7ad7f6d75e410
shiloh <lixiaolei595@gmail.com>
2025-07-17 16:10:21 +0800

    feat: add all services to IOC container

--- Avalonia.Dashboard.Services/Extensions/ServiceCollectionExtenstion.cs
+++ Avalonia.Dashboard.Services/Extensions/ServiceCollectionExtenstion.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Dashboard.Abstractions.Services;
+﻿using System.Reflection;
+using Avalonia.Dashboard.Common.Constants;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Avalonia.Dashboard.Services.Extensions;
@@ -8,11 +9,28 @@ namespace Avalonia.Dashboard.Services.Extensions;
 public static class ServiceCollectionExtenstion
 {
     /// <summary>
-    ///     注入服务
+    ///     Add all services to IOC container
     /// </summary>
-    /// <param name="serviceCollection"></param>
+    /// <param name="serviceCollection">
+    ///     <see cref="IServiceCollection" />
+    /// </param>
     public static void AddServices(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSingleton<IMenuService, MenuService>();
+        var serviceTypes = Assembly.GetAssembly(typeof(MenuService))!
+            .GetTypes()
+            .Where(t => t is { IsClass: true, IsAbstract: false } &&
+                        t.FullName!.EndsWith(GlobalConstants.ServiceClassNameSuffix))
+            .ToList();
+        foreach (var serviceType in serviceTypes)
+        {
+            var interfaces = serviceType.GetInterfaces();
+            if (interfaces.Length == 0)
+            {
+                serviceCollection.AddSingleton(serviceType);
+                continue;
+            }
+
+            foreach (var @interface in interfaces) serviceCollection.AddSingleton(@interface, serviceType);
+        }
     }
 }
